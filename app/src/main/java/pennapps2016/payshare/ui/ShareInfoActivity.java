d6fe228ead30695b0be03eb016a9484e56bc5a08
BenWu <bw96412@hotmail.com>
2016-01-23 15:55:21 -0500

    subtle style changes

---
+++ app/src/main/java/pennapps2016/payshare/ui/ShareInfoActivity.java
@@ -0,0 +1,9 @@
+package pennapps2016.payshare.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BenWu on 2016-01-23.
+ */
+public class ShareInfoActivity extends AppCompatActivity {
+}
