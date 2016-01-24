d7a67d9d56601d6bf6e37b1f6db436bc5d8fe0e0
BenWu <bw96412@hotmail.com>
2016-01-24 02:36:01 -0500

    fixes login bug and changed login animations

--- app/src/main/java/pennapps2016/payshare/ui/SplashActivity.java
+++ app/src/main/java/pennapps2016/payshare/ui/SplashActivity.java
@@ -1,6 +1,7 @@
 package pennapps2016.payshare.ui;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -45,7 +46,8 @@ public class SplashActivity extends Activity {
     private void startLoginActivity() {
         Log.d("SplashActivity", "Splash finished");
         Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        Bundle args = ActivityOptions.makeCustomAnimation(this, R.anim.fade_in_activity, R.anim.fade_out_activity).toBundle();
+        startActivity(intent, args);
         finish();
     }
 }
