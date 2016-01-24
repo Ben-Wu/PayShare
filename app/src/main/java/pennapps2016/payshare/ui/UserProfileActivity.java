5dfe477be34ef6eaf1065835d645c62592ab7d50
U-DESKTOP-L787FUB\David <info@mrdavidliu.me>
2016-01-23 21:53:49 -0500

    profile done

--- app/src/main/java/pennapps2016/payshare/ui/UserProfileActivity.java
+++ app/src/main/java/pennapps2016/payshare/ui/UserProfileActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import pennapps2016.payshare.R;
 import pennapps2016.payshare.utils.NetworkHelper;
@@ -29,7 +36,35 @@ public class UserProfileActivity extends AppCompatActivity{
         ((TextView)findViewById(R.id.username)).setText(pref.getString(LoginActivity.PREF_USER,"-1"));
 
         try {
-            JSONArray info  = new JSONArray(response);
+            JSONObject returned  = new JSONObject(response);
+            JSONArray info = returned.getJSONArray("data");
+            //debit
+            JSONArray debit = returned.getJSONArray("debit");
+            ArrayList<Holder> debitList = new ArrayList<>();
+            for (int i = 0; i<debit.length(); i++){
+                Holder holder = new Holder();
+                holder.cash = ""+debit.getJSONObject(i).getDouble("amount");
+                holder.color = (debit.getJSONObject(i).getString("payer_id").equals(pref.getString(LoginActivity.PREF_DEBIT,"-1")))?
+                        "red":"green";
+                holder.date = debit.getJSONObject(i).getString("transaction_date");
+                debitList.add(holder);
+            }
+            ((ListView)findViewById(R.id.debit_transaction)).setAdapter(new TransactionAdapter(debitList));
+            //credit
+            JSONArray credit = returned.getJSONArray("credit");
+            ArrayList<Holder> creditList = new ArrayList<>();
+            for (int i = 0; i<credit.length(); i++){
+                Holder holder = new Holder();
+                holder.cash = ""+credit.getJSONObject(i).getDouble("amount");
+                holder.color = (credit.getJSONObject(i).getString("payer_id").equals(pref.getString(LoginActivity.PREF_DEBIT,"-1")))?
+                        "red":"green";
+                if(!credit.getJSONObject(i).getString("status").equals("executed")){
+                    holder.color = "yellow";
+                }
+                holder.date = credit.getJSONObject(i).getString("transaction_date");
+                creditList.add(holder);
+            }
+            ((ListView)findViewById(R.id.credit_transaction)).setAdapter(new TransactionAdapter(creditList));
             for (int i = 0; i<info.length(); i++){
                 JSONObject object = info.getJSONObject(i);
                 if(object.getString("type").equals("Checking")){
@@ -42,5 +77,50 @@ public class UserProfileActivity extends AppCompatActivity{
             e.printStackTrace();
         }
 
+    }
+
+    private class Holder{
+        public String cash, color, date;
+    }
+
+    private class TransactionAdapter extends BaseAdapter{
+        ArrayList<Holder> list;
+
+        TransactionAdapter(ArrayList<Holder> array){
+            list = array;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+                convertView = inflater.inflate(R.layout.list_item_transaction,parent,false);
+            }
+            ((TextView)convertView.findViewById(R.id.price)).setText(list.get(position).cash);
+            ((TextView)convertView.findViewById(R.id.date)).setText(list.get(position).date);
+            if(list.get(position).color.equals("red")){
+                ((TextView)convertView.findViewById(R.id.price)).setTextColor(getResources().getColor(R.color.red));
+            }else if(list.get(position).color.equals("yellow")){
+                ((TextView)convertView.findViewById(R.id.price)).setTextColor(getResources().getColor(R.color.yellow));
+            }else{
+                ((TextView)convertView.findViewById(R.id.price)).setTextColor(getResources().getColor(R.color.green));
+            }
+            return convertView;
+        }
     }
 }
